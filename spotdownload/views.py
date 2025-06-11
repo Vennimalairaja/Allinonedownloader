870cd785abbdb595f6b52129453ed1d720960f0b
Raja <kvennimalairaja@gmail.com>
2025-06-11 22:10:46 +0530

    Completed half stage

--- spotdownload/views.py
+++ spotdownload/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from .forms import URLForm
+from django.http import HttpResponse
+from .spot import spot_download
 # Create your views here.
 def Homepage(request):
-    return render('index.html')
+    form=URLForm()
+    if request.method=='POST':
+        form=URLForm(request.POST)
+        if form.is_valid():
+            url=form.cleaned_data['url']
+            if spot_download(url):
+                return HttpResponse('<h1>Success</h1>')
+            else:
+                return HttpResponse('<h1>Failure</h1>')
+    context={'form':form}
+    return render(request,'index.html',context)
