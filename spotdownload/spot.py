0575c96aeee39935395cb5027db8816f68dcd8cd
Raja <kvennimalairaja@gmail.com>
2025-06-12 18:40:22 +0530

    80% completed

--- spotdownload/spot.py
+++ spotdownload/spot.py
@@ -1,8 +1,15 @@
 import subprocess
+import os
+import shutil
 def spot_download(url):
     try:
         result=subprocess.run(['spotdl',url],capture_output=True,text=True,check=True)
-        return True
+        file=''
+        for i in os.listdir('/home/raja/Documents/allinonedownloader'):
+            if i[-3:]=='mp3':
+                file=i
+                break
+        #shutil.move(f'/home/raja/Documents/allinonedownloader/{file}','/home/raja/Documents/allinonedownloader/static/Music')
+        return file
     except Exception as e:
         return False
-#print(spot_download('https://open.spotify.com/track/0nrJ7jsUFR0pDHV6NvKJje?si=b10c9df40de54fd3'))
--- spotdownload/views.py
+++ spotdownload/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import URLForm
 from django.http import HttpResponse
 from .spot import spot_download
+from .move import move_file
+from django.templatetags.static import static
 # Create your views here.
 def Homepage(request):
     form=URLForm()
@@ -9,9 +11,15 @@ def Homepage(request):
         form=URLForm(request.POST)
         if form.is_valid():
             url=form.cleaned_data['url']
-            if spot_download(url):
-                return HttpResponse('<h1>Success</h1>')
+            file=spot_download(url)
+            if file:
+                success_failure=move_file(file)
+                file=f'/Music/{file}'
+                link=static(file)
+                return render(request,'download.html',{'link':link})
             else:
                 return HttpResponse('<h1>Failure</h1>')
     context={'form':form}
     return render(request,'index.html',context)
+def download_page(request):
+    return render(request,'download.html')
